e55cd1a311e3c4d1bb36b69215e7bdcccded2c3d
Mike <penskimike@gmail.com>
2021-03-08 11:55:51 +0100

    update filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,33 +9,65 @@ const API_URL_POSTS = 'https://api.harvardartmuseums.org/object';
 const body = document.querySelector("body");
 const postContainer = document.querySelector("#posts .container .row");
 
-let classificationFilter = document.querySelector("#classification-filter");
+//filter
+const classificationFilter = document.querySelector("#classification-filter");
+const mediumFilter = document.querySelector("#medium-filter");
 
+const loadmoreButton = document.querySelector("#loadmore");
+const numberOfItemsContainer = document.querySelector("#number-items");
+const numberOfPages = document.querySelector("#item-pages");
+const currentPageContainer = document.querySelector("#current-page");
+
+let type;
+let currentPage = 1;
+
+//get - set query filter helper function
 var queryStringObject = {
-    classification: "Paintings|Prints",
-    //hasimage: 1,
-    //page: 1,
+    hasimage: 1,
+    //medium: "Terracotta",
+    classification: "Audiovisual Works",
+    page: 1,
     size: 10,
+    //&q=title:20,
 };
 
-
-classificationFilter.addEventListener("change", () => {
-
-    queryStringObject["classification"] = classificationFilter.value;
-
-    console.log(queryStringObject)
-
-    var queryString = JSON.stringify(queryStringObject);
-    queryString = "&" +queryString.replaceAll(':', '=').replaceAll(',', '&').replaceAll('"', '').slice(1, -1);
-
-    fetchPosts(queryString);
-});
 
 
 var queryString = JSON.stringify(queryStringObject);
 queryString = "&" +queryString.replaceAll(':', '=').replaceAll(',', '&').replaceAll('"', '').slice(1, -1);
+//console.log(queryString)
 
-console.log(queryString)
+/*
+* eventListener for classification
+*/
+classificationFilter.addEventListener("change", () => {
+
+    type = "classification";
+
+    queryStringObject["classification"] = classificationFilter.value;
+
+    var queryString = JSON.stringify(queryStringObject);
+    queryString = "&" +queryString.replaceAll(':', '=').replaceAll(',', '&').replaceAll('"', '').slice(1, -1);
+
+    fetchPosts(type, queryString, currentPage);
+});
+
+/*
+* eventListener for medium
+*/
+mediumFilter.addEventListener("change", () => {
+
+    type = "medium";
+
+    queryStringObject["medium"] = mediumFilter.value;
+    queryStringObject["classification"] = "any";
+
+    var queryString = JSON.stringify(queryStringObject);
+    queryString = "&" +queryString.replaceAll(':', '=').replaceAll(',', '&').replaceAll('"', '').slice(1, -1);
+
+    fetchPosts(type, queryString, currentPage);
+});
+
 
 /*
 * adding padding to body (header height)
@@ -52,9 +84,29 @@ resizeContent();
 window.onresize = resizeContent;
 
 
-let html = '';
+/*
+* fetch posts
+*/
 
-let fetchPosts = (queryString) => {
+let html = '';
+//only render pageNumber 1 time
+let pageFlag = 0;
+
+let fetchPosts = async (type, queryString, currentPage) => {
+
+    if(type == "classification" || "loadmore"){
+         html = '';
+    }
+
+    if(type == "classification"){
+        pageFlag = 0;
+        currentPage = 1;
+   }
+
+   if(type == "medium"){
+    pageFlag = 0;
+    currentPage = 1;
+    }
    
     fetch(API_URL_POSTS + API_KEY + queryString, {
         method: 'GET',
@@ -64,30 +116,36 @@ let fetchPosts = (queryString) => {
         
         let posts = data.records;
 
-        console.log(posts)
-
-        postContainer.innerHTML = "";
+         console.log(posts.length);
+        console.log(data); 
         
+        numberOfItemsContainer.innerHTML = `items: ${data.info.totalrecords}`;
+        
+        currentPageContainer.innerHTML = `${currentPage} / `;
+        
+        if(pageFlag == 0){
+            numberOfPages.innerHTML = `${data.info.pages}`;
+        }
+        
+
         posts.forEach(element => {
 
             html += `<div class="col-md-3 mb-4">`;
             html += `<img src="${element.primaryimageurl}" />`;
+            html += `<div clas="title">${element.title}</div>`;
             html += `</div>`;
 
-           
-
-            // Your existing code unmodified...
-            var iDiv = document.createElement('div');
+           /*  var iDiv = document.createElement('div');
             iDiv.className = 'col-md-3 mb-4';
             iDiv.innerHTML = `<img src="${element.primaryimageurl}" />`;
-            postContainer.appendChild(iDiv);
+            postContainer.appendChild(iDiv); */
             
         });
 
-       
-    //postContainer.innerHTML = " ";
-    //postContainer.innerHTML = html;
 
+    postContainer.innerHTML = html;
+
+    pageFlag = 1;
        
     })
     .catch((error) => {
@@ -95,5 +153,33 @@ let fetchPosts = (queryString) => {
     });
 }
 
-fetchPosts(queryString);
+fetchPosts(type, queryString, currentPage);
 
+
+
+/*
+* eventListener for loadmoreButton
+*/
+
+loadmoreButton.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    type = "loadmore";
+
+    console.log(currentPage, parseInt(numberOfPages.innerHTML))
+
+    if(currentPage < numberOfPages.innerHTML){
+
+        queryStringObject["size"] = queryStringObject["size"] + 10;
+
+        var queryString = JSON.stringify(queryStringObject);
+        queryString = "&" +queryString.replaceAll(':', '=').replaceAll(',', '&').replaceAll('"', '').slice(1, -1);
+
+        currentPage++;
+
+        fetchPosts(type, queryString, currentPage);
+
+    } 
+  
+});
